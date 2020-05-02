2cd5ecf9c05cc6ad140078e881fa750e9af245b9
Sushil <sushilt553@gmail.com>
2020-05-02 19:13:37 -0400

    Productify o(n)

--- productify.js
+++ productify.js
@@ -1,18 +1,18 @@
-// function productify(arr) {
+function productify(arr) {
 
-//     let newArr = [];
+    let newArr = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         let product = 1;
-//         for (let j = 0; j < arr.length; j++) {
-//             if (i !== j) {
-//                 product *= arr[j];
-//             }
-//         }
-//         newArr.push(product);
-//     }
-//     return newArr;
-// }
+    for (let i = 0; i < arr.length; i++) {
+        let product = 1;
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j) {
+                product *= arr[j];
+            }
+        }
+        newArr.push(product);
+    }
+    return newArr;
+}
 
 function productify(arr) {
     let product = arr.reduce((a, b) => a * b);
