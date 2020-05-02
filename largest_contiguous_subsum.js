a3c049bd7725cc0fa41b144cf18728d1fd4c5250
Sushil <sushilt553@gmail.com>
2020-05-02 18:14:34 -0400

    O(n) largest Contiguous sum

--- largest_contiguous_subsum.js
+++ largest_contiguous_subsum.js
@@ -1,26 +1,41 @@
+// function largestContiguousSubsum(arr) {
+//     let subArr = subarrays(arr);
+//     let max = -Infinity;
+
+//     subArr.forEach((arr) => {
+//         let subarrSum = arr.reduce((a, b) => a + b)
+//         if ( subarrSum > max) {
+//             max = subarrSum
+//         }
+//     })
+//     return max;
+// }
+
+// function subarrays(arr) {
+//     let subs = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j <= arr.length; j++) {
+//             let subarr = arr.slice(i, j);
+//             subs.push(subarr);
+//         }
+//     }
+//     return subs;
+// }
+
 function largestContiguousSubsum(arr) {
-    let subArr = subarrays(arr);
-    let max = -Infinity;
+    let max = arr[0] || 0;
+    let sum = 0;
 
-    subArr.forEach((arr) => {
-        let subarrSum = arr.reduce((a, b) => a + b)
-        if ( subarrSum > max) {
-            max = subarrSum
-        }
-    })
-    return max;
-}
-
-function subarrays(arr) {
-    let subs = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j <= arr.length; j++) {
-            let subarr = arr.slice(i, j);
-            subs.push(subarr);
+    for (let i = 1; i < arr.length; i++) {
+        sum += arr[i];
+        if (sum > max) {
+            max = sum;
+        }else if (sum < 0) {
+            sum = 0;
         }
     }
-    return subs;
+    return max;
 }
 
 console.log(largestContiguousSubsum([-1,10,3]));
