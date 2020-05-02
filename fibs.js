66d7f823dc2906b563543082ada938d640af4207
Sushil <sushilt553@gmail.com>
2020-05-01 21:07:49 -0400

    Fibs iterative done

--- fibs.js
+++ fibs.js
@@ -1,11 +1,23 @@
+// function fibs(num) {
+//     if (num === 0) return [];
+//     if (num === 1) return [0];
+//     if (num === 2) return [0, 1]
+
+//     let prevFibs = fibs(num - 1);
+//     prevFibs.push(prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]);
+//     return prevFibs;
+// }
+
 function fibs(num) {
     if (num === 0) return [];
     if (num === 1) return [0];
-    if (num === 2) return [0, 1]
+    
+    let fibsArr = [0, 1];
 
-    let prevFibs = fibs(num - 1);
-    prevFibs.push(prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]);
-    return prevFibs;
+    for (let i = 2; i < num; i++) {
+        fibsArr.push(fibsArr[i - 1] + fibsArr[i - 2]);
+    }
+    return fibsArr;
 }
 
 console.log(fibs(3));
