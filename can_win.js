d397a1852f8ed90ef21b910b8ff406b6c2cac321
Sushil <sushilt553@gmail.com>
2020-05-03 19:01:27 -0400

    Recursive and memoization logic for canWin algorithm

--- can_win.js
+++ can_win.js
@@ -21,5 +21,16 @@ function checkRight(arr, idx, val = 0) {
     return arr[index] === val;
 }
 
+//recursive memoization
+
+function canWin(arr, pos = 0, seen = {}) {
+    if (pos < 0 || pos >= arr.length || seen[pos]) return false;
+    if (arr[pos] === 0) return true;
+
+    seen[pos] = true;
+
+    return canWin(arr, pos + arr[pos], seen) || canWin(arr, pos - arr[pos], seen);
+}
+
 console.log(canWin([1,0,1], 0));
 console.log(canWin([1,2,0], 0));
