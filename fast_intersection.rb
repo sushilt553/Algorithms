535c3f931867dd82114e222bf55eb7de4e33d61e
Sushil <sushilt553@gmail.com>
2020-05-03 17:58:35 -0400

    Set use for intersection

--- fast_intersection.rb
+++ fast_intersection.rb
@@ -1,12 +1,20 @@
-def fast_intersection(arr1, arr2) {
-    intersection = [];
+def fast_intersection(arr1, arr2)
+    intersection = []
 
     arr1.each do |ele|
         if arr2.include?(ele)
             intersection.push(ele)
         end
     end
-    intersection;
-}
+    intersection
+end
 
+def fast_intersection(arr1, arr2)
+    set = arr1.to_set
+    intersection = []
+
+    arr2.each {|ele| intersection << ele if set.include?(ele)}
+
+    intersection
+end 
 
