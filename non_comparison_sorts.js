5ad58986662b6063975b26d8aafd494a59a63405
Sushil <sushilt553@gmail.com>
2020-05-03 19:21:19 -0400

    Counting sort algorithm

--- non_comparison_sorts.js
+++ non_comparison_sorts.js
@@ -8,4 +8,20 @@ function sortArr(arr) {
     return newArr;
 }
 
-console.log(sortArr([5,3,4,1,2]));
+function sortArr1(arr, max) {
+    let buckets = new Array(max + 1).fill(0);
+
+    arr.forEach(ele => buckets[ele]++);
+    
+    let newArr = [];
+    for (let i = 0; i < buckets.length; i++) {
+        while (buckets[i] > 0) {
+            newArr.push(i);
+            buckets[i]--;
+        }
+    }
+    return newArr;
+}
+
+// console.log(sortArr([5,3,4,1,2]));
+console.log(sortArr1([12,5,6,1,2], 12));
