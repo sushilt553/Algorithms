4fc74bb48dd83cbf19c414a329910d73c43df01e
Sushil <sushilt553@gmail.com>
2020-05-03 18:11:21 -0400

    common subset done

---
+++ common_subsets.rb
@@ -0,0 +1,18 @@
+def common_subsets(arr1, arr2) 
+    subs1 = subsets(arr1)
+    subs2 = subsets(arr2)
+
+    common = []
+    subs2.each {|ele| common << ele if subs1.include?(ele)}
+    common
+end
+
+def subsets(arr)
+    return [[]] if arr.empty? 
+    first = arr[0]
+    subset = subsets(arr.drop(1))
+    new_subs = subset.map {|ele| ele + [first]}
+    new_subs + subset
+end
+
+p common_subsets([1,2,3], [2,3])
