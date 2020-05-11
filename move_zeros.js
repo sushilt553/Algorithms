6743f0d01d894d05316ccdda0b5289a25ec414a4
Sushil <sushilt553@gmail.com>
2020-05-11 13:16:18 -0400

    Move zeros in O(n)

--- move_zeros.js
+++ move_zeros.js
@@ -1,3 +1,21 @@
 function moveZeros(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    let bool = true;
+
+    while (bool) {
+        while (arr[left] !== 0 && left !== right ) left++;
     
+        while (arr[right] === 0 && left !== right) right--;
+
+        if (left === right) {
+            bool = false;
+        }
+        
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+    }
+    return arr;
 }
+
+console.log(moveZeros([1,2,0,3,4,0,5,6,0]))
