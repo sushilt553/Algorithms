77dbc0486da5ac4145ee47b66c70a8838067a40b
Sushil <sushilt553@gmail.com>
2020-05-11 13:20:57 -0400

    LookandSay function done

---
+++ look_and_say.js
@@ -0,0 +1,16 @@
+function lookAndSay(arr) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let count = 1;
+        let j = i + 1;
+        while (arr[j] === arr[i]) {
+            count++;
+            j++;
+        }
+        newArr.push([count, arr[i]]);
+    }
+    return newArr;
+}
+
+console.log(lookAndSay([1]));
