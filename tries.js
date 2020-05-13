bc3d673933d6538d4aca6a2680a8e02bfeeb0e79
Sushil <sushilt553@gmail.com>
2020-05-13 18:35:22 -0400

    Tries implementation

--- tries.js
+++ tries.js
@@ -44,10 +44,10 @@ class Trie {
     }
 }
 
-let trie = new Trie();
-trie.insert("tea");
-trie.insert("tear");
-trie.insert("taco");
+// let trie = new Trie();
+// trie.insert("tea");
+// trie.insert("tear");
+// trie.insert("taco");
 
-console.log(trie.search("tea"));
-console.log(trie.search("tac"));
+// console.log(trie.search("tea"));
+// console.log(trie.search("tac"));
