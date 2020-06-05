3832bc7db15551063409108f6d56c935a420ba18
Sushil <sushilt553@gmail.com>
2020-06-05 13:45:40 -0400

    Find destination done

--- leetcode/destCity.js
+++ leetcode/destCity.js
@@ -1,3 +1,11 @@
 var destCity = function(paths) {
+    let hash = {};
 
+    for (let i = 0; i < paths.length; i++) {
+        hash[paths[i][0]] = true;
+    };
+
+    for (let i = 0; i < paths.length; i++) {
+        if (!hash[paths[i][1]]) return paths[i][1];
+    }
 }
