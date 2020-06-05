b3004d76683b77c8a8e463d5cfd866d1c2034574
Sushil <sushilt553@gmail.com>
2020-06-05 12:37:47 -0400

    Brute force solution of isPrefixOfWord

--- leetcode/isPrefixOfWord.js
+++ leetcode/isPrefixOfWord.js
@@ -2,10 +2,17 @@ var isPrefixOfWord = function(sentence, searchWord) {
     let arr = sentence.split(" ");
 
     for (let i = 0; i < arr.length; i++) {
-       if (arr[i].includes(searchWord)) return i + 1;
+       if (checkPrefix(arr[i], searchWord)) return i + 1
     }
     
     return -1;
+}
+
+function checkPrefix(str, target) {
+    for (let i = 0; i < target.length; i++) {
+        if (str[i] !== target[i]) return false;
+    }
+    return true;
 }
 
 console.log(isPrefixOfWord("I love eating burger", "burg"));
