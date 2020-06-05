108ccd3933443cfd70e65aa37310a088c6e34f2b
Sushil <sushilt553@gmail.com>
2020-06-05 12:17:57 -0400

    O(n) solution for maxProduct

--- leetcode/maxProduct.js
+++ leetcode/maxProduct.js
@@ -19,5 +19,17 @@ var maxProduct = function(nums) {
     return (sorted[sorted.length - 1] - 1) * (sorted[sorted.length - 2] - 1);
 }
 
+//O(n)
+var maxProduct = function(nums) {
+    let m1 = nums[0]; 
+    let m2 = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        m2 = Math.max(m2, Math.min(nums[i], m1));
+        m1 = Math.max(m1, nums[i]);
+    }
+    return (m1 - 1) * (m2 - 1);
+}
+
 console.log(maxProduct([3,4,5,2]));
 console.log(maxProduct([1,5,4,5]));
