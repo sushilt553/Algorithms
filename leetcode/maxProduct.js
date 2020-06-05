342b393394682aacab19d256888970a3397cd3c4
Sushil <sushilt553@gmail.com>
2020-06-05 12:05:59 -0400

    MaxProduct done

--- leetcode/maxProduct.js
+++ leetcode/maxProduct.js
@@ -1,3 +1,16 @@
 var maxProduct = function(nums) {
-    
+    let max = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            let num = (nums[i] - 1) * (nums[j] - 1);
+            if (num > max && j > i) {
+                max = num;
+            }
+        }
+    }
+    return max;
 }
+
+console.log(maxProduct([3,4,5,2]));
+console.log(maxProduct([1,5,4,5]));
