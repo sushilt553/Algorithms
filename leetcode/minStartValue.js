e352d7123c4f5fff489e69d434bf965927e6c724
Sushil <sushilt553@gmail.com>
2020-06-08 19:36:42 -0400

    Solve minStartValue in leetcode

--- leetcode/minStartValue.js
+++ leetcode/minStartValue.js
@@ -1,3 +1,22 @@
 const minStartValue = (nums) => {
-    
+    let minStartValue = 1;
+    let currentSum = minStartValue;
+
+    let loop = true;
+    while (loop) {
+        loop = false;
+
+        for (let i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            if (currentSum < 1) {
+                minStartValue += 1;
+                currentSum = minStartValue;
+                loop = true;
+                break;
+            }
+        }
+    }
+    return minStartValue;
 }
+
+console.log(minStartValue([-3, 2, -3, 4, 2]));
