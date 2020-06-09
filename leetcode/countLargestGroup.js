46a01dc484fc857137fd363805a0c0d07fa6c856
Sushil <sushilt553@gmail.com>
2020-06-09 15:20:51 -0400

    Helper function for countLargestGroup

--- leetcode/countLargestGroup.js
+++ leetcode/countLargestGroup.js
@@ -1,3 +1,39 @@
 const countLargestGroup = (n) => {
+    let hash = {};
+
+    for (let i = 1; i <= n; i++) {
+        let sum = findSum(i);
+        if (hash[sum]) {
+            hash[sum].push(i);
+        }else{
+            hash[sum] = [i];
+        }
+    }
     
+    let countHash = {};
+
+    for (let key in hash) {
+        let length = hash[key].length;
+
+        if (countHash[length]) {
+            countHash[length] += 1;
+        }else{
+            countHash[length] = 1;
+        }
+    }
+
+    return countHash[Math.max(...Object.keys(countHash))];
 }
+
+const findSum = (num) => {
+    let sum = 0;
+
+    while (num > 0) {
+        let n = num % 10;
+        sum += n;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+}
+
+console.log(countLargestGroup(13));
