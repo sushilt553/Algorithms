8705975328a210cf552264b59251a31e74dac4f4
Sushil <sushilt553@gmail.com>
2020-06-08 20:41:02 -0400

    Minsubsequence done

--- leetcode/minSubsequence.js
+++ leetcode/minSubsequence.js
@@ -1,30 +1,15 @@
 const minSubsequence = (nums) => {
+    nums = nums.sort((a, b) => b - a);
     let sum = nums.reduce((acc, ele) => acc + ele);
     let sequences = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j <= nums.length; j++) {
-            let subArr = nums.slice(i, j);
-            let currentSum = subArr.reduce((acc, ele) => acc + ele);
-            let remaining = sum;
-            subArr.forEach((ele) => remaining -= ele);
-            
-            if (currentSum > remaining) {
-                sequences.push(subArr);
-            }
-        }
+    let currentSum = 0;
+    while (currentSum <= sum) {
+        let value = nums.shift();
+        sequences.push(value);
+        currentSum += value
+        sum -= value;
     }
-
-    let minLength = sequences[0].length;
-    let subArr = sequences[0];
-
-    for (let i = 0; i < sequences.length; i++) {
-        if (sequences[i].length < minLength) {
-            minLength = sequences[i].length;
-            subArr = sequences[i];
-        }
-    }
-
-    return subArr.sort((a, b) => b - a);
+    return sequences;
 }
 
 console.log(minSubsequence([4, 3, 10, 9, 8]));
