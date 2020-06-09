8dd877841881759cd6e8cf65739d3fc23e332d1e
Sushil <sushilt553@gmail.com>
2020-06-09 16:53:37 -0400

    LuckyNumbers in matrix done

--- leetcode/luckyNumbers.js
+++ leetcode/luckyNumbers.js
@@ -1,29 +1,56 @@
-function luckyNumbers(matrix) {
-    let transposed = transposedArray(matrix);
-}
+//Works only for square matrix.
 
-function transposedArray(matrix) {
+// function luckyNumbers(matrix) {
+//     let transposed = transposedArray(matrix);
+
+//     let result = [];
+
+//     for (let i = 0; i < matrix.length; i++) {
+//         let min = Math.min(...matrix[i]);
+//         let max = Math.max(...transposed[i]);
+//         if (min === max) result.push(min);
+//     }
+//     return result;
+// }
+
+// function transposedArray(matrix) {
+//     let row = matrix.length;
+//     let col = matrix[0].length;
+//     let transposed = new Array(row).fill().map(() => new Array(col));
+
+//     for (let i = 0; i < row; i++) {
+//         for (let j = 0; j < col; j++) {
+//             transposed[j][i] = matrix[i][j];
+//         }
+//     }
+//     return transposed;
+// }
+
+const luckyNumbers = (matrix) => {
     let row = matrix.length;
     let col = matrix[0].length;
-    let transposed = new Array(row).fill().map(() => new Array(col));
+
+    let min = new Array(row).fill(Infinity);
+    let max = new Array(col).fill(-Infinity);
 
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            transposed[j][i] = matrix[i][j];
+            min[i] = Math.min(min[i], matrix[i][j]);
+            max[j] = Math.max(max[j], matrix[i][j]);
         }
     }
-    return transposed;
+    let result = [];
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (min[i] === max[j]) result.push(matrix[i][j]);
+        }
+    }
+
+    return result;
 }
 
 console.log(luckyNumbers([[7,8], [1,2]]));
-console.log(transposedArray([[7,8], [1,2]]));
+console.log(luckyNumbers([[3, 7, 8], [9, 11, 13], [15, 16, 17]]))
 
-[
-    [7,8],
-    [1,2]
-] //row Minimum
 
-[
-    [7,1],
-    [8,2]
-] //row Maximum
