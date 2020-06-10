6610429b3b310a2192156ac4bc0884a3b25d0a36
Sushil <sushilt553@gmail.com>
2020-06-10 12:58:05 -0400

    Leetcode sortstring

--- leetcode/sortString.js
+++ leetcode/sortString.js
@@ -1,36 +1,35 @@
 var sortString = function(s) {
-   let hash = {};
-   let result = "";
+    let hash = {};
+    let result = "";
 
-   for (let i = 0; i < s.length; i++) {
-       if (hash[s[i]]) {
-           hash[s[i]]++;
-       }else{
-           hash[s[i]] = 1;
-       }
-   }
+    for (let i = 0; i < s.length; i++) {
+        if (hash[s[i]]) {
+            hash[s[i]]++;
+        }else{
+            hash[s[i]] = 1;
+        }
+    }
 
-   let hasChars = () => {
-       return Object.keys(hash).some((key) => hash[key] > 0);
-   }
+    let hasChars = () => {
+        return Object.keys(hash).some((ele) => hash[ele] > 0);
+    }
 
-   let increasing = (m = true) => {
-       let k = Object.keys(hash).sort().filter((a) => hash[a] > 0);
+    let increasing = (m = true) => {
+        let k = Object.keys(hash).sort().filter((ele) => hash[ele] > 0);
+        k = (m === false) ? k.reverse() : k;
 
-       k = m === false ? k.reverse() : k;
+        k.forEach((ele) => {
+            result += ele;
+            hash[ele]--;
+        })
+    }
 
-       k.forEach((ele) => {
-           result += ele;
-           hash[ele]--;
-       })
-   }
-
-   while (hasChars()) {
-       increasing();
-       if (hasChars()) {
-           increasing(false);
-       }
-   }
-   return result;
+    while(hasChars()) {
+        increasing()
+        if (hasChars()) {
+            increasing(false);
+        }
+    }
+    return result;
 }
 
