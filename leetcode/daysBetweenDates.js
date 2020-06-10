3c5618d0c7860bc9a152f30a04f454e158b50737
Sushil <sushilt553@gmail.com>
2020-06-10 13:24:29 -0400

    Helper method for daysBetweenDays function

---
+++ leetcode/daysBetweenDates.js
@@ -0,0 +1,9 @@
+var daysBetweenDays = function(date1, date2) {
+    
+}
+
+var convertToArr = (date) => {
+    return date.split("-").map((n) => Number(n));
+}
+
+console.log(convertToArr("2019-06-29"))
