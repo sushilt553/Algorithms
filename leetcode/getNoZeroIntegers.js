c64adcbc8002aa246055f4bbb2d5010e5cf9abab
Sushil <sushilt553@gmail.com>
2020-06-11 16:48:57 -0400

    getNoZeroIntegers pass

--- leetcode/getNoZeroIntegers.js
+++ leetcode/getNoZeroIntegers.js
@@ -1,10 +1,14 @@
 var getNoZeroIntegers = function(n) {
     let result = [];
     let num = n - 1;
-    if (num % 10 === 0) {
-        num -= 1;
-        result.push(2); 
-        result.push(num);
+    if (checkZero(num)) {
+        let i = 1
+        while (checkZero(num) || checkZero(i)){
+            num -= 1;
+            ++i; 
+        }
+        result.push(i);
+        result.push(num)
     }else{
         result.push(1);
         result.push(num);
@@ -12,6 +16,15 @@ var getNoZeroIntegers = function(n) {
     return result;
 }
 
+var checkZero = (n) => {
+    let s = String(n);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "0") return true;
+    }
+    return false;
+}
+
 console.log(getNoZeroIntegers(2));
 console.log(getNoZeroIntegers(11));
 console.log(getNoZeroIntegers(10000));
+console.log(getNoZeroIntegers(1010));
