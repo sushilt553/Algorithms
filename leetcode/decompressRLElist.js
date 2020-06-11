5bcfab7a3b8cab61125c224b8fdc5e69b15e4068
Sushil <sushilt553@gmail.com>
2020-06-11 17:07:39 -0400

    decompressRLElist leetcode problem

--- leetcode/decompressRLElist.js
+++ leetcode/decompressRLElist.js
@@ -1,3 +1,16 @@
 var decompressRLElist = function (nums) {
+    let result = [];
 
+    for (let i = 0; i < nums.length; i++) {
+        let freq = nums[2 * i];
+        let val = nums[2 * i + 1];
+        while (freq > 0) {
+            result.push(val);
+            freq--;
+        }
+    }
+    return result;
 };
+
+console.log(decompressRLElist([1,2,3,4]));
+console.log(decompressRLElist([1,1,2,3]));
