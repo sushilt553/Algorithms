0d8c865ece1fc06509f76ba6cbfcddf5799bfc46
Sushil <sushilt553@gmail.com>
2020-06-12 11:51:17 -0400

    replaceElements leetcode problem

--- leetcode/replaceElements.js
+++ leetcode/replaceElements.js
@@ -1,3 +1,17 @@
 var replaceElements = function(arr) {
-    
+    let newArr = [];
+    let maxEle;
+    while (arr.length > 0) {
+        arr.shift();
+        maxEle = findMax(arr);
+        newArr.push(maxEle);
+    }
+    newArr[newArr.length - 1] = -1;
+    return newArr;
 }
+
+var findMax = function(arr) {
+    return Math.max(...arr);
+}
+
+console.log(replaceElements([17,18,5,4,6,1]));
