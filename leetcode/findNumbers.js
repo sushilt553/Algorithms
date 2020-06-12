9ea59b7387897af00953812ce568b8404bdb4990
Sushil <sushilt553@gmail.com>
2020-06-12 11:59:35 -0400

    CountEvens leetcode

--- leetcode/findNumbers.js
+++ leetcode/findNumbers.js
@@ -2,8 +2,23 @@ var findNumbers = function(nums) {
     let count = 0;
 
     nums.forEach((ele) => {
-        if (ele % 2 === 0) count++;
+        if (countEvens(ele)) count++;
     })
 
     return count;
 }
+
+var countEvens = function(n) {
+    let count = 0;
+    while (n > 0) {
+        count++;
+        n = Math.floor(n / 10);
+    }
+    if (count % 2 === 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(findNumbers([12,345,2,6,7896]));
