4e6d6a21b1d5a65023844aad0affbeafdc73af73
Sushil <sushilt553@gmail.com>
2020-06-17 14:19:45 -0400

    removeNthFromNode problem done

--- TopInterviewQuestions/removeNthFromEnd.js
+++ TopInterviewQuestions/removeNthFromEnd.js
@@ -1,3 +1,39 @@
-var removeNthFromEnd = function(head, n) {
-    
+class Node {
+    constructor(val, next) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
 }
+
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let length = listLength(head);
+    let currHead = dummy;
+    length -= n;
+
+    while (length > 0) {
+        currHead = currHead.next;
+        length--;
+    }
+    dummy
+    return dummy.next;
+}
+
+var listLength = function(head) {
+    let length = 0;
+
+    while (head) {
+        length++;
+        head = head.next;
+    }
+    return length
+}
+
+let head = new Node(4);
+let nextNode = new Node(5);
+let nextnextNode = new Node(6);
+
+head.next = nextNode;
+nextNode.next = nextnextNode;
+console.log(removeNthFromEnd(head, 1));
