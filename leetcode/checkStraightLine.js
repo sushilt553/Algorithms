d17738bb74c5fdd1b4c2806ef4b6f92c5e537b63
Sushil <sushilt553@gmail.com>
2020-06-19 16:43:38 -0400

    checkStraightLine problem in leetcode

--- leetcode/checkStraightLine.js
+++ leetcode/checkStraightLine.js
@@ -1,23 +1,38 @@
 var checkStraightLine = function(coordinates) {
-    let x;
-    let y;
-    let nextX;
-    let nextY;
-
+    if (coordinates.length <=  2) return true;
     let result = true;
 
     for (let i = 0; i < coordinates.length - 1; i++) {
-        x = coordinates[i][0];
-        y = coordinates[i][1];
-        nextX = coordinates[i + 1][0];
-        nextY = coordinates[i + 1][1];
-
-        if (nextX !== x + 1 || nextY !== y + 1) {
+        if (coordinates[i][0] !== coordinates[i+1][0]) {
             result = false;
             break;
         }
     }
-    return result;
+
+    if (result) return result;
+
+    for (let i = 0; i < coordinates.length - 1; i++) {
+        if (coordinates[i][1] !== coordinates[i+1][1]) {
+            result = false;
+            break;
+        }
+    }
+    
+    if (result) return result;
+
+    let x1 = coordinates[0][0];
+    let x2 = coordinates[1][0];
+    let y1 = coordinates[0][1];
+    let y2 = coordinates[1][1];
+
+    let slope = (y2 - y1) / (x2 - x1);
+
+    for (let i = 2; i < coordinates.length; i++) {
+        x2 = coordinates[i][0];
+        y2 = coordinates[i][1];
+        if ((y2 - y1) / (x2 - x1) !== slope) return false;
+    }
+    return true;
 }
 
 console.log(checkStraightLine([[1,2], [2,3], [3,4], [4,5], [5,6], [6,7]]))
