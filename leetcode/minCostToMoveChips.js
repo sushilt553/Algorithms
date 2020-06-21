dc8c5644ca99dc8ce39afb9f3f35ca5281948499
Sushil <sushilt553@gmail.com>
2020-06-21 14:31:36 -0400

    minOddandEven

--- leetcode/minCostToMoveChips.js
+++ leetcode/minCostToMoveChips.js
@@ -1,3 +1,14 @@
 var minCostToMoveChips = function(chips) {
-    
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i < chips.length; i++) {
+        if (chips[i] % 2 === 0) {
+            even++;
+        }else{
+            odd++;
+        }
+    }
+
+    return Math.min(even, odd);
 }
