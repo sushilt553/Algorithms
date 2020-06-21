923ac0a1f0f52173756b3eefe6de05a384f1f70d
Sushil <sushilt553@gmail.com>
2020-06-21 14:53:51 -0400

    minAbsolute difference problem leetcode

--- leetcode/minimumAbsDifferences.js
+++ leetcode/minimumAbsDifferences.js
@@ -1,3 +1,20 @@
 var minAbsoluteDifference = function(arr) {
+    let sortedArr = arr.sort((a, b) => a - b);
+
+    let minValue = sortedArr[1] - sortedArr[0];
+    let result = [];
     
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            let ele = Math.abs(arr[j] - arr[i]);
+
+            if (ele === minValue && j > i) {
+                result.push([arr[i], arr[j]]);
+            }
+        }
+    }
+    return result;
 }
+
+
+console.log(minAbsoluteDifference([4,2,1,3]))
