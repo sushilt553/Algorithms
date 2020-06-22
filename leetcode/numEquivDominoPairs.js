60f11f5399d7d4569ecf042025ba8f02bbca6a83
Sushil <sushilt553@gmail.com>
2020-06-22 17:45:40 -0400

    numEquivDomino pairs n2 solution

--- leetcode/numEquivDominoPairs.js
+++ leetcode/numEquivDominoPairs.js
@@ -1,3 +1,16 @@
 function numEquivDominoPairs(dominoes) {
-    
+    let count = 0;    
+    for (let i = 0; i < dominoes.length; i++) {
+        for (let j = 0; j < dominoes.length; j++) {
+            if (j > i) {
+                let first = dominoes[i][0];
+                let second = dominoes[i][1];
+                let third = dominoes[j][0];
+                let fourth = dominoes[j][1];
+
+                if ((first === third && second === fourth) || (first === fourth && second === third)) count++;
+            }
+        }
+    }
+    return count;
 }
