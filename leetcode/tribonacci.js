629ef334fa50c41bfb3ef38cfec900a714b2b6ba
Sushil <sushilt553@gmail.com>
2020-06-22 17:25:11 -0400

    Tribonacci using memoization

---
+++ leetcode/tribonacci.js
@@ -0,0 +1,15 @@
+function tribonacci(n, memo = {}) {
+    if (memo[n]) return memo[n];
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 1;
+
+    memo[n - 1] = tribonacci((n - 1), memo); 
+    memo[n - 2] = tribonacci((n - 2), memo);
+    memo[n - 3] = tribonacci((n - 3), memo);
+
+    return memo[n - 1] + memo[n - 2] + memo[n - 3]
+}
+
+console.log(tribonacci(4));
+console.log(tribonacci(25));
