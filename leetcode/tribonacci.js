3c6fea989e3575edb3bcf0203096724affb82966
Sushil <sushilt553@gmail.com>
2020-06-22 17:30:28 -0400

    numEquivDominoPairs function

--- leetcode/tribonacci.js
+++ leetcode/tribonacci.js
@@ -4,12 +4,14 @@ function tribonacci(n, memo = {}) {
     if (n === 1) return 1;
     if (n === 2) return 1;
 
-    memo[n - 1] = tribonacci((n - 1), memo); 
-    memo[n - 2] = tribonacci((n - 2), memo);
-    memo[n - 3] = tribonacci((n - 3), memo);
-
-    return memo[n - 1] + memo[n - 2] + memo[n - 3]
+    memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+    return memo[n];
 }
 
 console.log(tribonacci(4));
 console.log(tribonacci(25));
+
+// memo[n - 1] = tribonacci((n - 1), memo);
+// memo[n - 2] = tribonacci((n - 2), memo);
+// memo[n - 3] = tribonacci((n - 3), memo);
+// return memo[n - 1] + memo[n - 2] + memo[n - 3]
