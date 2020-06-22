22c45a3da47cec2dc45491b4568f284e71ef4282
Sushil <sushilt553@gmail.com>
2020-06-22 16:31:05 -0400

    numSmallerByFrequency leetcode problem

---
+++ leetcode/numSmallerByFrequency.js
@@ -0,0 +1,39 @@
+function numSmallerByFrequency (queries, words) {
+    let newQueries = queries.map((ele) => findSmallestValue(ele));
+    let newWords = words.map((ele) => findSmallestValue(ele));
+
+    let result = [];
+
+    for (let i = 0; i < newQueries.length; i++) {
+        let count = 0;
+        for (let j = 0; j < newWords.length; j++) {
+            if (newQueries[i] < newWords[j]) count++;
+        }
+        result.push(count);
+    }
+    return result;
+}
+
+function findSmallestValue(str) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let char = str[0];
+    let index = alphabet.indexOf(char);
+    for (let i = 1; i < str.length; i++) {
+        let idx = alphabet.indexOf(str[i]);
+        if (idx < index) {
+            char = str[i];
+            index = idx;
+        }
+    }
+
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) count++;
+    }
+
+    return count;
+}
+
+console.log(numSmallerByFrequency(["cbd"], ["zaaaz"]))
+console.log(numSmallerByFrequency(["bbb", "cc"], ["a", "aa", "aaa", "aaaa"]))
