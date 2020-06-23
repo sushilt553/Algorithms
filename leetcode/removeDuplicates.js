92faefdd88ea3ed045870bf2fbb3e1c7e460bbc0
Sushil <sushilt553@gmail.com>
2020-06-23 16:56:33 -0400

    removeDuplicates splice method

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -1,3 +1,18 @@
 var removeDuplicates = function(S) {
-    
+
+    let i = 0
+    while (i < S.length) {
+        if (S[i] === S[i + 1]) {
+            S = S.split("");
+            S.splice(i, 2);
+            S = S.join("");
+            i = 0;
+        }else{
+            i++;
+        }
+    }
+
+    return S;
 }
+
+console.log(removeDuplicates("abbaca"));
