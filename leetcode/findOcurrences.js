50840edf93813d4e8d3c01d38de74664b4e3bcbd
Sushil <sushilt553@gmail.com>
2020-06-23 15:38:17 -0400

    findOcurrences problem in leetcode solution

--- leetcode/findOcurrences.js
+++ leetcode/findOcurrences.js
@@ -1,3 +1,17 @@
 var findOcurrences = function(text, first, second) {
-    
+    text = text.split(" ");
+    let result = [];
+
+    for (let i = 0; i < text.length - 2; i++) {
+        let word = text[i];
+
+        if (word === first && text[i + 1] === second) {
+            result.push(text[i + 2]);
+        }
+    }
+
+    return result;
 }
+
+console.log(findOcurrences("alice is a good girl she is a good student", "a", "good"))
+console.log(findOcurrences("we will we will rock you", "we", "will"))
