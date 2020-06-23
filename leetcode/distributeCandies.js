75198535d0c5f514774005b0078db4afeb5a70c5
Sushil <sushilt553@gmail.com>
2020-06-23 13:46:21 -0400

    Solve distributeCandies problem

--- leetcode/distributeCandies.js
+++ leetcode/distributeCandies.js
@@ -1,3 +1,18 @@
 var distributeCandies = function(candies, num_people) {
-    
+    let arr =  new Array(num_people).fill(0);
+
+    let i = 0;
+    let c = 1;
+    while (candies > 0) {
+        if (candies <= c) c = candies;
+        arr[i] += c;
+        candies -= c;
+        i = (i + 1) % num_people;
+        c++;
+    }
+
+    return arr;
 }
+
+console.log(distributeCandies(10, 4))
+console.log(distributeCandies(10, 3))
