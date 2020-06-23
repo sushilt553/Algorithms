161a107b09e6f1332f2ed7415f69fa30c9fe3d86
Sushil <sushilt553@gmail.com>
2020-06-23 16:59:57 -0400

    removeDuplicates method using stack

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -15,4 +15,17 @@ var removeDuplicates = function(S) {
     return S;
 }
 
+var removeDuplicates = function(S) {
+    let stack = [];
+
+    for (let i = 0; i < S.length; i++) {
+        if (S[i] !== stack[stack.length - 1]){
+            stack.push(S[i]);
+        }else{
+            stack.pop();
+        }
+    }
+    return stack.join("")
+}
+
 console.log(removeDuplicates("abbaca"));
