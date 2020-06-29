d9382223866d11287e39d3dd5066a51fc20679b6
Sushil <sushilt553@gmail.com>
2020-06-29 18:02:26 -0400

    Stack project complete

--- app_academy_quest/stack_project/lib/stack.js
+++ app_academy_quest/stack_project/lib/stack.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(val) {
+        let node = new Node(val);
+        if (this.length === 0) {
+            this.top = node;
+            this.bottom = node;
+        }else{
+            let temp = this.top;
+            this.top = node;
+            this.top.next = temp;
+        }
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        if (this.length === 0) return null;
+        if (this.length === 1) {
+            let temp = this.top;
+            this.top = null;
+            this.bottom = null;
+            this.length--;
+            return temp.value;
+        }else{
+            let temp = this.top;
+            this.top = temp.next;
+            this.length--;
+            return temp.value;
+        }
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
