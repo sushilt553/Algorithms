5d2e787e580eae02888c55359bc16451c5890651
Sushil <sushilt553@gmail.com>
2020-06-29 13:09:26 -0400

    Binary search iteration

--- app_academy_quest/binary_search_project/lib/binary_search.js
+++ app_academy_quest/binary_search_project/lib/binary_search.js
@@ -1,17 +1,37 @@
+// function binarySearch(array, target) {
+//     if (array.length === 0) return false;
+
+//     let midIdx = Math.floor(array.length / 2);
+//     let leftArr = array.slice(0, midIdx);
+//     let rightArr = array.slice(midIdx + 1);
+
+//     if (array[midIdx] > target) {
+//         return binarySearch(leftArr, target);
+//     }else if (array[midIdx] < target) {
+//         return binarySearch(rightArr, target);
+//     }else{
+//         return true
+//     }
+// }
+
 function binarySearch(array, target) {
     if (array.length === 0) return false;
 
-    let midIdx = Math.floor(array.length / 2);
-    let leftArr = array.slice(0, midIdx);
-    let rightArr = array.slice(midIdx + 1);
+    let leftIdx = 0;
+    let rightIdx = array.length;
 
-    if (array[midIdx] > target) {
-        return binarySearch(leftArr, target);
-    }else if (array[midIdx] < target) {
-        return binarySearch(rightArr, target);
-    }else{
-        return true
+    while (leftIdx <= rightIdx) {
+        let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+        if (array[midIdx] > target) {
+            rightIdx = midIdx - 1;
+        }else if (array[midIdx] < target) {
+            leftIdx = midIdx + 1;
+        }else if (array[midIdx] === target) {
+            return true;
+        }
     }
+    return false;
 }
 
 function binarySearchIndex(array, target) {
