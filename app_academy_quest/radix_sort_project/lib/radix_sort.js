4daee5c63a0bb0761cfc0928e72064b7690c24bf
Sushil <sushilt553@gmail.com>
2020-06-29 13:51:11 -0400

    Radix sort complete

--- app_academy_quest/radix_sort_project/lib/radix_sort.js
+++ app_academy_quest/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,40 @@
+function findNumber(num, place) {
+    return Number(String(num).split("").reverse()[place]);
+}
+
+function numberLength(num) {
+    return String(num).length;
+}
+
+function maxLength(arr) {
+    let max = numberLength(arr[0]);
+
+    for (let i = 1; i < arr.length; i++) {
+        let currentLength = numberLength(arr[i]);
+        if (currentLength > max) max = currentLength;
+    }
+
+    return max;
+}
+
 function radixSort(arr) {
 
+    if (!Array.isArray(arr)) return null;
+
+    for (let i = 0; i < maxLength(arr); i++) {
+        let buckets = new Array(10).fill().map(() => new Array());
+        for (let j = 0; j < arr.length; j++) {
+            if (numberLength(arr[j]) > i) {
+                let num = findNumber(arr[j], i);
+                buckets[num].push(arr[j]);
+            }else{
+                buckets[0].push(arr[j]);
+            }
+        }
+        arr = [].concat(...buckets);
+    }
+
+    return arr;
 }
 
 module.exports = {
