e0ba422030033fbbdd7c644f62cde9cfe15a2995
Sushil <sushilt553@gmail.com>
2020-06-29 13:29:22 -0400

    Counting sort complete

--- app_academy_quest/counting_sort_project/lib/counting_sort.js
+++ app_academy_quest/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,20 @@
 function countingSort(arr, max) {
+    if (arr.length <= 1) return arr;
 
+    let buckets = new Array(max + 1).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+        buckets[arr[i]]++;
+    }
+    let result = [];
+
+    for (let i = 0; i < buckets.length; i++) {
+        while (buckets[i] > 0) {
+            result.push(i);
+            buckets[i]--;
+        }
+    }
+    return result;
 }
 
 
