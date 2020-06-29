491d432ba206afaff617d2d548dad69d2d661459
Sushil <sushilt553@gmail.com>
2020-06-29 13:22:52 -0400

    Insertion sort complete

--- app_academy_quest/insertion_sort_project/lib/insertion_sort.js
+++ app_academy_quest/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,13 @@
 function insertionSort(arr) {
 
+    for (let i = 1; i < arr.length; i++) {
+        let currentEle = arr[i];
+        for (var j = i - 1; j >= 0 && currentEle < arr[j]; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentEle;
+    }
+    return arr;
 }
 
 module.exports = {
