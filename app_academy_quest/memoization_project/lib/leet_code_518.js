282b93d0fec9875e11ac90fdbbb1da0a3c5ed615
Sushil <sushilt553@gmail.com>
2020-06-29 15:39:55 -0400

    Memoization solution for change

--- app_academy_quest/memoization_project/lib/leet_code_518.js
+++ app_academy_quest/memoization_project/lib/leet_code_518.js
@@ -2,6 +2,8 @@
 // Feel free to use this file for scratch work.
 
 var change = function(amount, coins, memo = {}) {
+    let key = `${coins} - ${amount}`
+    if (memo[key]) return memo[key];
 
     if (amount === 0) return 1;
 
@@ -10,8 +12,9 @@ var change = function(amount, coins, memo = {}) {
     let count = 0;
 
     for (let quantity = 0; amount >= quantity * currCoin; quantity++) {
-        count += change(amount - quantity * currCoin, coins.slice(0, -1))
+        count += change(amount - quantity * currCoin, coins.slice(0, -1), memo)
     }
 
-    return count;
+    memo[key] = count;
+    return memo[key];
 }
