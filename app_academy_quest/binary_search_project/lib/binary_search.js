7392fba5f64ed0b3f76b2dc09c7f36579edc949e
Sushil <sushilt553@gmail.com>
2020-06-29 12:56:03 -0400

    Binary search recursion complete

--- app_academy_quest/binary_search_project/lib/binary_search.js
+++ app_academy_quest/binary_search_project/lib/binary_search.js
@@ -1,5 +1,17 @@
 function binarySearch(array, target) {
+    if (array.length === 0) return false;
 
+    let midIdx = Math.floor(array.length / 2);
+    let leftArr = array.slice(0, midIdx);
+    let rightArr = array.slice(midIdx + 1);
+
+    if (array[midIdx] > target) {
+        return binarySearch(leftArr, target);
+    }else if (array[midIdx] < target) {
+        return binarySearch(rightArr, target);
+    }else{
+        return true
+    }
 }
 
 function binarySearchIndex(array, target) {
