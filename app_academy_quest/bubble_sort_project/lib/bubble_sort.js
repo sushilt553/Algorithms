691edf99ef7cc4a8709553eb18e388e66d2d41b7
Sushil <sushilt553@gmail.com>
2020-06-29 12:44:18 -0400

    Bubble sort complete

--- app_academy_quest/bubble_sort_project/lib/bubble_sort.js
+++ app_academy_quest/bubble_sort_project/lib/bubble_sort.js
@@ -1,11 +1,26 @@
 function swap(array, idx1, idx2) {
-    let temp = array[idx1]
-    array[idx1] = array[idx2]
-    array[idx2] = temp
+    let temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
+    return array
 }
 
 function bubbleSort(array) {
 
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true;
+
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                sorted = false;
+            }
+        }
+    }
+
+    return array;
 }
 
 
