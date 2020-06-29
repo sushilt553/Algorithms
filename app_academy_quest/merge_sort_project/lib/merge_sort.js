bd1e66c743f06cd5117bd4f7143493d47bde3d3f
Sushil <sushilt553@gmail.com>
2020-06-29 12:47:38 -0400

    merge helper function

--- app_academy_quest/merge_sort_project/lib/merge_sort.js
+++ app_academy_quest/merge_sort_project/lib/merge_sort.js
@@ -1,5 +1,20 @@
 function merge(array1, array2) {
+    let merged = [];
 
+    while (array1.length || array2.length) {
+        let first = array1.length > 0 ? array1[0] : Infinity;
+        let second = array2.length > 0 ? array2[0] : Infinity;
+
+        let next;
+
+        if (first < second) {
+            next = array1.shift();
+        }else{
+            next = array2.shift();
+        }
+        merged.push(next);
+    }
+    return merged;
 }
 
 function mergeSort(array) {
