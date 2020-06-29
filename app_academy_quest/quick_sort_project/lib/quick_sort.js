af1f5d3486e257c88340c153cd7317183abd430c
Sushil <sushilt553@gmail.com>
2020-06-29 12:52:02 -0400

    Quick sort complete

--- app_academy_quest/quick_sort_project/lib/quick_sort.js
+++ app_academy_quest/quick_sort_project/lib/quick_sort.js
@@ -1,4 +1,11 @@
 function quickSort(array) {
+    if (array.length <= 1) return array;
+
+    let pivotEle = array.shift();
+    let leftArr = array.filter((ele) => ele < pivotEle);
+    let rightArr = array.filter((ele) => ele >= pivotEle);
+
+    return quickSort(leftArr).concat([pivotEle], quickSort(rightArr));
 
 }
 
