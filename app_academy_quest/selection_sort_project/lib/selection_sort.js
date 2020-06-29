bd0622b44a319c8d59988eb381833b45a6ec5cb0
Sushil <sushilt553@gmail.com>
2020-06-29 13:19:32 -0400

    Selection sort complete

--- app_academy_quest/selection_sort_project/lib/selection_sort.js
+++ app_academy_quest/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,20 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return arr;
 }
 
 function selectionSort(arr) {
 
+    for (let i = 0; i < arr.length; i++) {
+        let minIdx = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIdx]) minIdx = j;
+        }
+        swap(arr, i, minIdx);
+    }
+    return arr;
 }
 
 module.exports = {
