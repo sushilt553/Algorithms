250565c954ace79535088fa113c10748b787ae27
Sushil <sushilt553@gmail.com>
2020-06-30 13:44:50 -0400

    TreeSum complete

--- app_academy_quest/depth_breadth_first_project/lib/tree_sum.js
+++ app_academy_quest/depth_breadth_first_project/lib/tree_sum.js
@@ -1,7 +1,24 @@
+// function treeSum(root) {
+//     if (!root) return 0;
+
+//     let sum = 0;
+//     let stack = [root];
+
+//     while (stack.length) {
+//         let node = stack.pop();
+//         sum += node.val;
+//         if (node.right) stack.push(node.right);
+//         if (node.left) stack.push(node.left);
+//     }
+
+//     return sum;
+// }
+
 function treeSum(root) {
+    if (!root) return 0;
 
+    return root.val + treeSum(root.left) + treeSum(root.right);
 }
-
 
 module.exports = {
     treeSum
