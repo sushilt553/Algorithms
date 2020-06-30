fc908a1d8b61130c16c3a5bc909adee08f0458d0
Sushil <sushilt553@gmail.com>
2020-06-30 17:45:29 -0400

    getParent method for heap

---
+++ app_academy_quest/heaps_project/lib/leetcode.js
@@ -0,0 +1,15 @@
+function findKthLargest(nums, k) {
+    let sorted = nums.sort((a, b) => a - b);
+
+    return sorted[nums.length - k];
+}
+
+class MaxHeap {
+    constructor() {
+        this.array = [null];
+    }
+
+    getParent(i) {
+        return Math.floor(i / 2);
+    }
+}
