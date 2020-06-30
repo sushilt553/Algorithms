4a427416c2a837c69e9521bbc601f670e0559974
Sushil <sushilt553@gmail.com>
2020-06-30 17:43:11 -0400

    isMaxHeap complete

--- app_academy_quest/heaps_project/lib/is_heap.js
+++ app_academy_quest/heaps_project/lib/is_heap.js
@@ -1,6 +1,17 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+    if (array[idx] === undefined) return true; 
 
+    let leftIdx = idx * 2;
+    let rightIdx = idx * 2 + 1;
+
+    let leftVal = array[leftIdx];
+    let rightVal = array[rightIdx];
+
+    if (!leftVal) leftVal = -Infinity;
+    if (!rightVal) rightVal = -Infinity;
+
+    return array[idx] > leftVal && array[idx] > rightVal && isMaxHeap(array, leftIdx) && isMaxHeap(array, rightIdx);
 }
 
 
