b34cee5ed0129fc8f02e696b72b9d47a80f83a38
Sushil <sushilt553@gmail.com>
2020-06-30 16:54:40 -0400

    maxValue complete

--- app_academy_quest/graph_project/lib/max_value.js
+++ app_academy_quest/graph_project/lib/max_value.js
@@ -1,5 +1,18 @@
-function maxValue(node, visited=new Set()) {
+function maxValue(node) {
+    let visited = new Set();
 
+    let stack = [node];
+    let maxValue = -Infinity;
+    while (stack.length) {
+        let node = stack.pop();
+        if (visited.has(node.val)) continue;
+        visited.add(node.val);
+        if (node.val > maxValue) maxValue = node.val;
+
+        stack.push(...node.neighbors);
+    }
+
+    return maxValue;
 }
 
 module.exports = {
