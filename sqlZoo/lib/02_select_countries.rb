dbe8d3cabee6c741b43eead9534d538562b92e3f
Sushil <sushilt553@gmail.com>
2020-06-30 19:22:19 -0400

    United we stand query

--- sqlZoo/lib/02_select_countries.rb
+++ sqlZoo/lib/02_select_countries.rb
@@ -64,5 +64,8 @@ end
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE name LIKE '%United%'
   SQL
 end
