d1b31b268aa16367dac8e5876a41ac6818f66d8f
Sushil <sushilt553@gmail.com>
2020-06-30 13:41:42 -0400

    BFS and DFS complete"

--- app_academy_quest/depth_breadth_first_project/lib/breadth_first_array.js
+++ app_academy_quest/depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,16 @@
 function breadthFirstArray(root) {
+    if (!root) return null;
 
+    let queue = [root];
+    let array = [];
+
+    while (queue.length) {
+        let node = queue.shift();
+        array.push(node.val);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return array;
 }
 
 module.exports = {
