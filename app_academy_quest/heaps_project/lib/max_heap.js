90147b7ea322e9a1993580051e1721841aec8886
Sushil <sushilt553@gmail.com>
2020-06-30 17:29:13 -0400

    DeleteMax complete

--- app_academy_quest/heaps_project/lib/max_heap.js
+++ app_academy_quest/heaps_project/lib/max_heap.js
@@ -38,7 +38,14 @@ class MaxHeap {
     }
 
     deleteMax() {
+        if (this.array.length === 2) return this.array.pop();
+        if (this.array.length === 1) return null;
 
+        let max = this.array[1];
+        this.array[1] = this.array.pop();
+
+        this.siftDown(1);
+        return max;
     }
 }
 
