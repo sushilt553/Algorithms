1fb4a68c63f147025977706378ba1c9e5fc74cb2
Sushil <sushilt553@gmail.com>
2020-06-30 16:51:59 -0400

    BFS for graph complete

--- app_academy_quest/graph_project/lib/breadth_first_search.js
+++ app_academy_quest/graph_project/lib/breadth_first_search.js
@@ -1,5 +1,17 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    let visited = new Set();
 
+    let queue = [startingNode];
+
+    while (queue.length) {
+        let node = queue.shift();
+        if (visited.has(node.val)) continue;
+        visited.add(node.val)
+        if (node.val === targetVal) return node;
+
+        queue.push(...node.neighbors);
+    }
+    return null;
 }
 
 module.exports = {
