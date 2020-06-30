297dbefd4e3fac792338f66d537444911afff364
Sushil <sushilt553@gmail.com>
2020-06-30 17:22:37 -0400

    Heap implementation

--- app_academy_quest/heaps_project/lib/max_heap.js
+++ app_academy_quest/heaps_project/lib/max_heap.js
@@ -1,5 +1,35 @@
 class MaxHeap {
-    
+    constructor() {
+        this.array = [null];
+    }
+
+    getParent(i) {
+        return Math.floor(i / 2);
+    }
+
+    getLeftChild(i) {
+        return i * 2;
+    }
+
+    getRightChild(i) {
+        return i * 2 + 1;
+    }
+
+    siftUp(i) {
+
+    }
+
+    insert(val) {
+
+    }
+
+    siftDown(i) {
+
+    }
+
+    deleteMax() {
+        
+    }
 }
 
 module.exports = {
