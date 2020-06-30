ec0aa70b9c81eb7267772b68f5ac10dbc99ac81f
Sushil <sushilt553@gmail.com>
2020-06-30 17:01:26 -0400

    NumRegions problem complete

--- app_academy_quest/graph_project/lib/num_regions.js
+++ app_academy_quest/graph_project/lib/num_regions.js
@@ -1,5 +1,25 @@
 function numRegions(graph) {
+    let visited = new Set();
+    let count = 0;
 
+    for (let node in graph) {
+        if (dfs(node, visited, graph)) count++;
+    }
+    return count;
+}
+
+function dfs(source, visited, graph) {
+    if (visited.has(source)) return false;
+
+    let stack = [source];
+    while (stack.length) {
+        let node = stack.pop();
+        if (visited.has(node)) continue;
+        visited.add(node);
+
+        stack.push(...graph[node]);
+    }
+    return true;
 }
 
 module.exports = {
