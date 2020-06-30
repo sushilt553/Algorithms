693112c27c189d3ee0ff8a7a7dcc0bee7fe5264b
Sushil <sushilt553@gmail.com>
2020-06-30 14:25:06 -0400

    isBalanced complete

--- app_academy_quest/bst_project/lib/leet_code_110.js
+++ app_academy_quest/bst_project/lib/leet_code_110.js
@@ -3,5 +3,15 @@
 
 
 function isBalanced(root) {
+    if (!root) return true;
 
+    let heightDifference = Math.abs(height(root.left) - height(root.right)) <= 1;
+
+    return heightDifference && isBalanced(root.left) && isBalanced(root.right);
+}
+
+function height(node) {
+    if (!node) return -1;
+
+    return 1 + Math.max(height(node.left), height(node.right));
 }
