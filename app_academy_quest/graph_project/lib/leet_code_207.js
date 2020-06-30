7583cd30c246827b49d5b89229e337a532baf6ee
Sushil <sushilt553@gmail.com>
2020-06-30 17:16:32 -0400

    Course schedule complete

--- app_academy_quest/graph_project/lib/leet_code_207.js
+++ app_academy_quest/graph_project/lib/leet_code_207.js
@@ -2,5 +2,41 @@
 //  https://leetcode.com/problems/course-schedule/
 
 function canFinish(numCourses, prerequisites) {
+    let prereq = createGraph(prerequisites);
+    let count = Object.keys(prereq.length);
 
+    let completedCourses = new Set();
+
+    let loop = true;
+    
+    while (loop) {
+        loop = false;
+        for (let course in prereq) {
+            let allPrereqComplete = prereq[course].every((pre) => completedCourses.has(pre));
+            if (!completedCourses.has(course) && allPrereqComplete) {
+                completedCourses.add(course);
+                loop = true;
+            }
+        }
+    }
+    return completedCourses.size === count;
+}
+
+function createGraph(prerequisites) {
+    let graph = {};
+
+    prerequisites.forEach((prereq) => {
+        let [course, pre] = prereq;
+
+        if (graph[course]) {
+            graph[course].push(pre);
+        }else{
+            graph[course] = [pre];
+        }
+
+        if (!graph[pre]) {
+            graph[pre] = [];
+        }
+    })
+    return graph;
 }
