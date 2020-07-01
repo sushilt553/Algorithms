f0898eef2c7fe3367040b5a49dd8391495d1d1d8
Sushil <sushilt553@gmail.com>
2020-07-01 13:29:38 -0400

    Animal table and insert values complete

--- psql/my_tables.sql
+++ psql/my_tables.sql
@@ -4,4 +4,17 @@ CREATE TABLE countries(
     population INTEGER NOT NULL
 )
 
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    country_id INTEGER NOT NULL,
+
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+)
+
+INSERT INTO 
+    countries
+    (name, population)
+VALUES
+    ('United States', 327000000)
 
