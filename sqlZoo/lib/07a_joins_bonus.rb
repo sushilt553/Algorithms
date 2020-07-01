16c8e36266cbf2b9347f228fd203fb12bb655daf
Sushil <sushilt553@gmail.com>
2020-07-01 12:43:08 -0400

    Alison artist sql query

--- sqlZoo/lib/07a_joins_bonus.rb
+++ sqlZoo/lib/07a_joins_bonus.rb
@@ -26,6 +26,10 @@ require_relative './sqlzoo.rb'
 def alison_artist
   # Select the name of the artist who recorded the song 'Alison'.
   execute(<<-SQL)
+  select albums.artist
+  from albums
+  join tracks on tracks.album = albums.asin
+  where tracks.song = 'Alison'
   SQL
 end
 
