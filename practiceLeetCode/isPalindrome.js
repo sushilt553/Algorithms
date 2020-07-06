e339c52a1e4cc8b2eb8e3575d8352e9983874386
Sushil <sushilt553@gmail.com>
2020-07-06 15:52:43 -0400

    Optimal solution for isPalindrome

--- practiceLeetCode/isPalindrome.js
+++ practiceLeetCode/isPalindrome.js
@@ -14,6 +14,18 @@ function isPalindrome(x) {
     }
 }
 
+function isPalindrome(x) {
+    if ( x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversed = 0;
+    while (x > reversed) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return reversed === x || x === Math.floor(reversed / 10);
+}
+
 console.log(isPalindrome(121));
 console.log(isPalindrome(1211));
 console.log(isPalindrome(-121));
