ba842a71725c77d0c1d758a59a37dc0612411571
Sushil <sushilt553@gmail.com>
2020-07-08 14:34:45 -0400

    One liner for isPalindrome

--- pairboarding/isPalindrome.js
+++ pairboarding/isPalindrome.js
@@ -1,14 +1,18 @@
+// function isPalindrome(str) {
+//     let start = 0;
+//     let end = str.length - 1;
+
+//     while (start <= end) {
+//         if (str[start] !== str[end]) return false;
+//         start++;
+//         end--;
+//     }
+
+//     return true;
+// }
+
 function isPalindrome(str) {
-    let start = 0;
-    let end = str.length - 1;
-
-    while (start <= end) {
-        if (str[start] !== str[end]) return false;
-        start++;
-        end--;
-    }
-
-    return true;
+    return str.split("").reverse().join("") === str;
 }
 
 console.log(isPalindrome("a"))
