4d7f64cee4332368be2395846cd86e6fd43ae24e
Sushil <sushilt553@gmail.com>
2020-07-08 15:40:38 -0400

    Largest contiguous sum O(n)

--- pairboarding/largest_contiguous_subsum.js
+++ pairboarding/largest_contiguous_subsum.js
@@ -1,31 +1,43 @@
+// function largestContiguousSum(arr) {
+//     let subArrays = findSubArray(arr);
+
+//     let result = [];
+//     let resultSum = 0;
+//     for (let i = 0; i < subArrays.length; i++) {
+//         let sum = findSum(subArrays[i]);
+//         if (sum > resultSum) {
+//             result = subArrays[i];
+//             resultSum = sum;
+//         }
+//     }
+//     return result;
+// }
+
+// function findSubArray(array) {
+//     let subs = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j <= array.length; j++) {
+//             subs.push(array.slice(i, j));
+//         }
+//     }
+//     return subs;
+// }
+
+// function findSum(arr) {
+//     return arr.reduce((acc, ele) => acc + ele);
+// }
+
 function largestContiguousSum(arr) {
-    let subArrays = findSubArray(arr);
+    let currentSum = 0;
+    let maxSum = arr[0] || 0;
 
-    let result = [];
-    let resultSum = 0;
-    for (let i = 0; i < subArrays.length; i++) {
-        let sum = findSum(subArrays[i]);
-        if (sum > resultSum) {
-            result = subArrays[i];
-            resultSum = sum;
-        }
+    for (let i = 0; i < arr.length; i++) {
+        currentSum += arr[i];
+        if (maxSum < currentSum) maxSum = currentSum;
+        if (currentSum < 0) currentSum = 0;
     }
-    return result;
+    return maxSum;
 }
 
-function findSubArray(array) {
-    let subs = [];
-
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j <= array.length; j++) {
-            subs.push(array.slice(i, j));
-        }
-    }
-    return subs;
-}
-
-function findSum(arr) {
-    return arr.reduce((acc, ele) => acc + ele);
-}
-
-console.log(largestContiguousSum([1,3,-1,-2]))
+console.log(largestContiguousSum([1,-1,-2, 20]))
