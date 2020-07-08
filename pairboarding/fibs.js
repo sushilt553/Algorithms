08912e04bb702faaf259f481d16c18b14322d7e7
Sushil <sushilt553@gmail.com>
2020-07-08 14:29:43 -0400

    Fibs iterative done

--- pairboarding/fibs.js
+++ pairboarding/fibs.js
@@ -1,10 +1,22 @@
+// const fibs = function(num) {
+//     if (num === 0) return [];
+//     if (num === 1) return [0];
+//     if (num === 2) return [0, 1];
+
+//     let prevFibs = fibs(num - 1);
+//     prevFibs.push(prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]);
+//     return prevFibs;
+// }
+
 const fibs = function(num) {
     if (num === 0) return [];
     if (num === 1) return [0];
-    if (num === 2) return [0, 1];
 
-    let prevFibs = fibs(num - 1);
-    prevFibs.push(prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]);
+    let prevFibs = [0, 1];
+
+    for (let i = 2; i < num; i++) {
+        prevFibs.push(prevFibs[i - 1] + prevFibs[i - 2]);
+    }
     return prevFibs;
 }
 
