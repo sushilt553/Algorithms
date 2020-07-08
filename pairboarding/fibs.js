ccc3840b81662b0170a070e02082f7f703d4e642
Sushil <sushilt553@gmail.com>
2020-07-08 14:26:59 -0400

    Fibs recursion

---
+++ pairboarding/fibs.js
@@ -0,0 +1,11 @@
+const fibs = function(num) {
+    if (num === 0) return [];
+    if (num === 1) return [0];
+    if (num === 2) return [0, 1];
+
+    let prevFibs = fibs(num - 1);
+    prevFibs.push(prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2]);
+    return prevFibs;
+}
+
+console.log(fibs(4))
