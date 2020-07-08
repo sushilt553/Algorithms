460d3512398f2ebbe6849bca723452a6b5cf5e3d
Sushil <sushilt553@gmail.com>
2020-07-08 14:59:34 -0400

    Shuffle using index

--- pairboarding/shuffle.js
+++ pairboarding/shuffle.js
@@ -1,5 +1,15 @@
+// function shuffle(arr) {
+//     return arr.sort(() => Math.random() - 0.5);
+// }
+
 function shuffle(arr) {
-    return arr.sort(() => Math.random() - 0.5);
+    let newArr = new Array(...arr);
+
+    for (let i = 0; i < arr.length; i++) {
+        let newIdx = Math.floor(Math.random() * arr.length);
+        [newArr[i], newArr[newIdx]] = [newArr[newIdx], newArr[i]];
+    }
+    return newArr;
 }
 
 console.log(shuffle([1,2,3,4,5]))
