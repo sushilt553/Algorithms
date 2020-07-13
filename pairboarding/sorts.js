f38a1ab6d068a10a10fca8e03631a6a6015e9e5c
Sushil <sushilt553@gmail.com>
2020-07-13 18:04:44 -0400

    Radix sort for strings

--- pairboarding/sorts.js
+++ pairboarding/sorts.js
@@ -25,5 +25,36 @@ const sort2 = (arr) => {
     return sortedArr;
 }
 
+function sort3(arr) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let sortedArr = [];
+
+    for (let i = 0; i < longestStr(arr); i++) {
+        let buckets = new Array(27).fill().map(() => new Array());
+        for (let j = 0; j < arr.length; j++) {
+            if (i < arr[j].length) {
+                let index = alphabet.indexOf(arr[j][arr[j].length - 1 - i]) + 1;
+                buckets[index].push(arr[j]);
+            }else{
+                buckets[0].push(arr[j]);
+            }
+        }
+        sortedArr = [].concat(...buckets);
+    }
+    return sortedArr;
+}
+
+function longestStr(arr) {
+    let max = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i].length) {
+            max = arr[i].length;
+        }
+    }
+    return max;
+}
+
 console.log(sort1([1,3,2,4,7,6,5]));
 console.log(sort2([1,3,2,4,7,6,5]));
+console.log(sort3(["abcd","ef", "ghi"]));
