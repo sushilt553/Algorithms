a68a1a5586fea4e027b012d97b4e9bf48ef52b9e
Sushil <sushilt553@gmail.com>
2020-07-13 17:05:23 -0400

    Counting sort solution

--- pairboarding/sorts.js
+++ pairboarding/sorts.js
@@ -6,4 +6,24 @@ const sort1 = (arr) => {
     return sortedArr;
 }
 
+const sort2 = (arr) => {
+    let max = Math.max(...arr);
+    let buckets = new Array(max + 1).fill(0);
+
+    for (let i = 0; i <= arr.length; i++) {
+        buckets[arr[i]]++;
+    }
+
+    let sortedArr = [];
+
+    for (let i = 0; i < buckets.length; i++) {
+        while (buckets[i]) {
+            sortedArr.push(i);
+            buckets[i]--;
+        }
+    }
+    return sortedArr;
+}
+
 console.log(sort1([1,3,2,4,7,6,5]));
+console.log(sort2([1,3,2,4,7,6,5]));
