c584469a4fe1f1b5263c32d3af2f742eaa00cec3
Sushil <sushilt553@gmail.com>
2020-07-14 16:50:59 -0400

    Weighted Random index complete

--- pairboarding/weighted_random_index.js
+++ pairboarding/weighted_random_index.js
@@ -1,3 +1,15 @@
 function weightedRandomIndex(arr) {
     let sum = arr.reduce((acc, ele) => acc + ele);
+    const val = Math.floor(Math.random() * sum);
+
+    let cumulativeSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        cumulativeSum += arr[i];
+
+        if (val < cumulativeSum) return i;
+    }
+    return cumulativeSum;
 }
+
+console.log(weightedRandomIndex([4,6,8]))
